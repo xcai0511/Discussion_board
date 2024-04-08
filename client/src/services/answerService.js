55478d6b77d36511c58d9f3c67a91d24dc8ce008
Xiaojin Cai <xcai0511@gmail.com>
2024-04-08 15:41:47 -0400

    services

---
+++ client/src/services/answerService.js
@@ -0,0 +1,20 @@
+import { REACT_APP_API_URL, api } from "./config";
+
+const ANSWER_API_URL = `${REACT_APP_API_URL}/answer`;
+
+// To add answer
+const addAnswer = async (qid, ans) => {
+    const data = { qid: qid, ans: ans };
+    const res = await api.post(`${ANSWER_API_URL}/addAnswer`, data);
+
+    return res.data;
+};
+
+// To edit answer
+const editAnswer = async(qid, ans) => {
+    const data = {qid: qid, ans: ans};
+    const res = await api.post(`${ANSWER_API_URL}/editAnswer`, data)
+    return res.data;
+}
+
+export { addAnswer, editAnswer };
