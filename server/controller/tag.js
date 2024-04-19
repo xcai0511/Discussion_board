2e2d168c37b95f346d7935faad58887eda7b67d9
jaimesi <si.ja@northeastern.edu>
2024-04-18 22:57:43 -0400

    splitting tag controller into smaller, more readable functions

--- server/controller/tag.js
+++ server/controller/tag.js
@@ -7,46 +7,56 @@ const router = express.Router();
 const getTagsWithQuestionNumber = async (req, res) => {
     try {
         const tags = await Tag.find();
-        //console.log("tags: ", tags);
-        if (tags[0]._id) {
-            const tagQuestionCounts = new Map(tags.map(tag => [tag._id.toString(), 0]));
+        const tagQuestionCounts = getTagQuestionCounts(tags);
+        const tagsWithCounts = mapTagsToCounts(tags, tagQuestionCounts);
 
-            const questions = await Question.find().populate('tags');
-            questions.forEach(question => {
-                question.tags.forEach(tag => {
-                    tagQuestionCounts.set(tag._id.toString(), (tagQuestionCounts.get(tag._id.toString()) || 0) + 1);
-                });
-            });
-
-            const tagsWithCounts = Array.from(tagQuestionCounts.keys()).map(tagId => ({
-                name: tags.find(tag => tag._id.toString() === tagId).name,
-                qcnt: tagQuestionCounts.get(tagId)
-            }));
-
-            res.json(tagsWithCounts);
-        } else {
-            const questions = await Question.find().populate('tags');
-            let tagCounts = {};
-            questions.forEach(question => {
-                question.tags.forEach(tag => {
-                    if (tag.name in tagCounts) {
-                        tagCounts[tag.name] += 1;
-                    } else {
-                        tagCounts[tag.name] = 1;
-                    }
-                });
-            });
-            const tagsWithCounts = Object.keys(tagCounts).map(tagName => ({
-                name: tagName,
-                qcnt: tagCounts[tagName]
-            }));
-            res.json(tagsWithCounts);
-        }
-
+        res.json(tagsWithCounts);
     } catch (e) {
         console.error("Error getting tags with question number:", e);
         res.status(500).send({ message: "Internal server error" });
     }
+};
+
+const getTagQuestionCounts = async (tags) => {
+    const tagQuestionCounts = new Map(tags.map(tag => [tag._id.toString(), 0]));
+    const questions = await Question.find().populate('tags');
+
+    questions.forEach(question => {
+        question.tags.forEach(tag => {
+            tagQuestionCounts.set(tag._id.toString(), (tagQuestionCounts.get(tag._id.toString()) || 0) + 1);
+        });
+    });
+
+    return tagQuestionCounts;
+};
+
+const mapTagsToCounts = (tags, tagQuestionCounts) => {
+    if (tags[0]._id) {
+        return Array.from(tagQuestionCounts.keys()).map(tagId => ({
+            name: tags.find(tag => tag._id.toString() === tagId).name,
+            qcnt: tagQuestionCounts.get(tagId)
+        }));
+    } else {
+        const tagCounts = countTags(tags);
+        return Object.keys(tagCounts).map(tagName => ({
+            name: tagName,
+            qcnt: tagCounts[tagName]
+        }));
+    }
+};
+
+const countTags = (tags) => {
+    const tagCounts = {};
+    tags.forEach(question => {
+        question.tags.forEach(tag => {
+            if (tag.name in tagCounts) {
+                tagCounts[tag.name] += 1;
+            } else {
+                tagCounts[tag.name] = 1;
+            }
+        });
+    });
+    return tagCounts;
 };
 
 // add appropriate HTTP verbs and their endpoints to the router.
