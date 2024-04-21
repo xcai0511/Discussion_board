a6a8230ca10709dd634450faafff6c3a9d1dfdd2
Xiaojin Cai <xcai0511@gmail.com>
2024-04-21 00:03:18 -0400

    navbar cypress component

--- client/cypress/component/sidebar.cy.js
+++ client/cypress/component/sidebar.cy.js
@@ -1,6 +1,6 @@
 import SideNavBar from '../../src/components/main/sideBarNav/index';
 
-it('should have question, tag, and saved post tab', () => {
+it('should have all the tabs', () => {
     const selected = '';
     const handleQuestionsSpy = cy.spy().as('handleQuestionsSpy');
     const handleTagSpy = cy.spy().as('handleTagSpy');
@@ -12,7 +12,10 @@ it('should have question, tag, and saved post tab', () => {
                 handleSavedPosts={handleSavedPostsSpy}/>)
     cy.get('#menu_question').contains('Questions');
     cy.get('#menu_tag').contains('Tag');
-    cy.get('#menu_savedPosts').contains('Saved Posts');
+    cy.get('#menu_savedPosts').contains('Saved');
+    cy.get('#menu_users').contains('Users');
+    cy.get('#menu_profile').contains('Profile');
+
 })
 
 it('Tabs should have been called', () => {
@@ -20,15 +23,23 @@ it('Tabs should have been called', () => {
     const handleQuestionsSpy = cy.spy().as('handleQuestionsSpy');
     const handleTagSpy = cy.spy().as('handleTagSpy');
     const handleSavedPostsSpy = cy.spy().as('handleSavedPostsSpy');
+    const handleUsersSpy = cy.spy().as('handleUsersSpy');
+    const handleProfileSpy = cy.spy().as('handleProfileSpy');
     cy.mount(<SideNavBar
         selected={selected}
         handleQuestions={handleQuestionsSpy}
         handleTags={handleTagSpy}
-        handleSavedPosts={handleSavedPostsSpy}/>)
+        handleSavedPosts={handleSavedPostsSpy}
+        handleUsers={handleUsersSpy}
+        handleProfile={handleProfileSpy}/>)
     cy.get('#menu_question').click();
     cy.get('@handleQuestionsSpy').should('have.been.called');
     cy.get('#menu_tag').click();
     cy.get('@handleTagSpy').should('have.been.called');
     cy.get('#menu_savedPosts').click();
     cy.get('@handleSavedPostsSpy').should('have.been.called');
+    cy.get('#menu_users').click();
+    cy.get('@handleUsersSpy').should('have.been.called');
+    cy.get('#menu_profile').click();
+    cy.get('@handleProfileSpy').should('have.been.called');
 })
