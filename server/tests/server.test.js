53121f25074ab0aa191374081969e146dd0074f3
Xiaojin Cai <xcai0511@gmail.com>
2024-04-22 18:54:07 -0400

    fix server jest tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const { default: mongoose } = require("mongoose");
 const User = require('../models/users');
 const { verifyPassword } = require("../utils/password");
+const supertest = require("supertest");
 
 jest.mock("../utils/password", () => ({
   verifyPassword: jest.fn()
@@ -9,28 +10,27 @@ jest.mock("../utils/password", () => ({
 jest.mock('../models/users');
 let server;
 describe('Session management tests', () => {
+  let connectSidValue = null;
+  let respToken;
+  let token;
   beforeEach(async () => {
-    server = require('../server');
-  })
-
-  afterEach(async() => {
-    server.close();
-    await mongoose.disconnect()
-  });
-
-  it('POST /login should return 200 if the user is valid and has a token and session', async () => {
-    // Get CSRF token
-    const respToken = await request(server).get('/auth/csrf-token');
-
-    const token = respToken.body.csrfToken;
-
-    // Extract the session ID from the response headers
-    let connectSidValue = null;
+    server = require("../server");
+    // Obtain CSRF token
+    respToken = await supertest(server).get('/auth/csrf-token');
+    token = respToken.body.csrfToken;
     respToken.headers['set-cookie'].forEach(cookie => {
       if (cookie.includes('connect.sid')) {
         connectSidValue = cookie.split('=')[1].split(';')[0];
       }
     });
+  });
+
+  afterEach(() => {
+    server.close();
+    mongoose.disconnect();
+  });
+
+  it('POST /login should return 200 if the user is valid and has a token and session', async () => {
 
     // Ensure the CSRF token was obtained successfully
     expect(respToken.status).toBe(200);
@@ -52,20 +52,6 @@ describe('Session management tests', () => {
 
 
   it('POST /login must return 403 forbidden if the user is valid but has no token', async () => {
-    // Get CSRF token
-    const respToken = await request(server).get('/auth/csrf-token');
-
-    // Extract the session ID from the response headers
-    let connectSidValue = null;
-    respToken.headers['set-cookie'].forEach(cookie => {
-      if (cookie.includes('connect.sid')) {
-        connectSidValue = cookie.split('=')[1].split(';')[0];
-      }
-    });
-
-    // Ensure the CSRF token was obtained successfully
-    expect(respToken.status).toBe(200);
-
     const fakeUser = { username: 'user1', password: 'password1' };
     const loginResponse = await request(server)
       .post('/auth/login')
@@ -75,14 +61,6 @@ describe('Session management tests', () => {
   });
 
   it('POST /login must return 403 forbidden if the user is valid but has no session', async () => {
-    // Get CSRF token
-    const respToken = await request(server)
-    .get('/auth/csrf-token');
-
-    const token = respToken.body.csrfToken;
-
-    // Ensure the CSRF token was obtained successfully
-    expect(respToken.status).toBe(200);
 
     const fakeUser = { username: 'user1', password: 'password1' };
     const respLogin = await request(server)
@@ -94,24 +72,8 @@ describe('Session management tests', () => {
   });
 
   it('/logout should successfully destroy the session', async () => {
-    // Get CSRF token
-    const respToken = await request(server).get('/auth/csrf-token');
-
-    let connectSidValue = null;
-    respToken.headers['set-cookie'].forEach(cookie => {
-      if (cookie.includes('connect.sid')) {
-        connectSidValue = cookie.split('=')[1].split(';')[0];
-      }
-    });
-  
-    const token = respToken.body.csrfToken;
-  
-    // Ensure the CSRF token was obtained successfully
-    expect(respToken.status).toBe(200);
-  
     // Login to establish a session
     const fakeUser = { username: 'user1', contactemail: 'test@email.com', password: 'testpassword' };
-    User.create.mockResolvedValueOnce(fakeUser);
     User.findOne = jest.fn().mockResolvedValueOnce(fakeUser);
     verifyPassword.mockResolvedValue(true);
     const loginResponse =  await request(server)
@@ -140,9 +102,6 @@ describe('Session management tests', () => {
     // Get CSRF token
     const respToken = await request(server)
     .get('/auth/csrf-token');
-
-    const setCookieHeader = respToken.headers['set-cookie'][0];
-    sessionId = setCookieHeader.split(';')[0].split('=')[1];
 
     const token = respToken.body.csrfToken;
 
